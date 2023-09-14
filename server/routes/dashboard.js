28048fc4f32d67ade18a825fc5e2f67dc20dd1b9
Varadpatil04 <110057020+Varadpatil04@users.noreply.github.com>
2023-09-15 01:44:24 +0530

    Made Chnages

--- server/routes/dashboard.js
+++ server/routes/dashboard.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const router = express.Router();
+const { isLoggedIn } = require('../middleware/checkingauth');
 const dashcontroller = require('../controller/dashcontroller');
 
 
-router.get('/dashboard',dashcontroller.dashboard);
+router.get('/dashboard', isLoggedIn ,dashcontroller.dashboard);
 
 
 module.exports = router;
